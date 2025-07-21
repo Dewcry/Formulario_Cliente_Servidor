a58822962bc7ef2bdfa571328eec9d797fc4e525
Dew <dewcry00@gmail.com>
2025-07-20 20:52:27 -0400

    Se añadio al serve.js POST, GET, PUT, PATCH

--- server.js
+++ server.js
@@ -1,58 +1,67 @@
 const http = require("http");
+const client = require('./db');
 
-/**const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end("!Hola mundo desde el servidor Node.Js--------->");
-})*/
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    let body = '';
+    //Esto escucha el evento data del objeto req, los datos de body
+    //llegan en pedazos por lo que los sumamos
+    req.on('data', chunk => body += chunk.toString());
 
-/**const server = http.createServer((req, res) => {
-    console.log("Todo lo que llega al REQ", req);
+    req.on('end', ()=>{
+        //Apartado POST para crear un nuevo contacto
+        if (method === 'POST' && url ==='/contacto'){
+            const data = JSON.parse(body);
+            const {nombre,correo,mensaje} = data;
 
-    if(req.url === '/') {
-        res.end("Pagina de inicio");
-    } else if(req.url === '/contacto') {
-        res.end("Pagina de contacto")
-    } else {
-        res.writeHead(404);
-        res.end("Pagina no encontrada");
-    }
-})*/
-
-/**const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type' : 'text/html'});
-    res.end(`
-        <html>
-            <head><title>252-web</title></head>
-            
-            <body>
-                <h1>Bienvenido a 252</h1>
-                <p>Servidor nodeJs con Html!!!!</p>
-            </body>
-        </html>
-        `);
-})*/
-
-const server = http.createServer((req, res) => { 
-    if(req.method === 'GET' && req.url === "/") {
-        res.writeHead(200, {"content-type": "text/json"});
-        const data = {
-            "saludo": "Hola desde mi servidor te envio un JSON",
-            "ciudad": "La Paz"
+            if (!nombre || !correo || !mensaje){
+                        res.writeHead(400);
+                        return res.end(JSON.stringify({error: 'Datos incompletos'}))
+                    }
+                    //Aca lo insertamos en la data base
+                    client.query('insert into constactos (nombre, correo, mensaje) values($1, $2, $3)',[nombre, correo, mensaje])
+                    .then(() => {
+                        res.writeHead(201, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify({mensaje:'Registrado con exito'}));
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.writeHead(500, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify({error: 'Error al hacer submit'}));
+                    });
+        //GET para listar los contactos
+        }else if(method === 'GET' && url === '/contacto'){
+            client.query('select * from constactos order by id DESC')
+            .then(result => {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(result.rows));
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.writeHead(500, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify({error: 'Error al listar'}));
+                });
+        //PUT Editar el contacto
+        }else if(method === 'PUT' && url === '/constacto'){
+            const id = url.split('/').pop();
+            const {nombre, correo, mensaje}=JSON.parse(body)||{};
+            if (!nombre || !correo || !mensaje){
+                res.writeHead(400, {'Content-Type':'application/jason'});
+                return res.end(JSON.stringify({error: 'Error al editar'}));
+            }
+            client.query('update contactos set nombre=$1, correo=$2, mensaje=$3 where id=$4',[nombre,correo,mensaje,id])
+            .then(()=>{
+                res.writeHead(200, {'Content-Type':'application/jason'});
+                return res.end(JSON.stringify({error: 'Se edito correctamente'}));
+            })
+            .catch(err=>{
+                res.writeHead(500, {'Content-Type':'application/jason'});
+                return res.end(JSON.stringify({error: 'Error al editar'}));
+            })
+        //PATCH actuliza unicamente el mensaje
         }
-
-        res.end(JSON.stringify(data));
-    } else if(req.method === 'POST' && req.url === "/registro") {
-         res.end(JSON.stringify({mensaje: "se creo correctamente"}));
-    }  else if(req.method === 'PUT' && req.url === "/actualizar") {
-         res.end(JSON.stringify({mensaje: "se actualizo correctamente la data"}));
-    } else if(req.method === 'DELETE' && req.url === "/eliminar") {
-         res.end(JSON.stringify({mensaje: "se elimino correctamente"}));
-    } else if(req.method === 'PATCH' && req.url === "/actualiza") {
-         res.end(JSON.stringify({mensaje: "se actualizo el dato correctamente"}));
-    } else {
-        console.warn("Error de Metodo u URL");
-    }
-
+    })
 })
 
 server.listen(3000, () => {
